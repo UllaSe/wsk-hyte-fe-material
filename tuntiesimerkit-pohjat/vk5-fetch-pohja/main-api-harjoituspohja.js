5b8810bbd1d72b66a38ceacf78cedb5185c8face
Ulla Sederlöf <ulla.sederlof@metropolia.fi>
2024-02-13 15:16:43 +0200

    rajapintahaku

--- tuntiesimerkit-pohjat/vk5-fetch-pohja/main-api-harjoituspohja.js
+++ tuntiesimerkit-pohjat/vk5-fetch-pohja/main-api-harjoituspohja.js
@@ -5,3 +5,72 @@ import { setupCounter } from './counter.js';
 import { fetchData } from './fetch.js';
 
 document.querySelector('#app').innerHTML = 'Moi tässä oman APIn harjoituksia';
+
+const bt1 = document.querySelector('.get_users');
+bt1.addEventListener('click', getAllUsers);
+
+async function getAllUsers() {
+  console.log('toimii!');
+
+  try {
+    const response = await fetch('http://127.0.0.1:3000/api/users');
+    console.log(response);
+    const data = await response.json();
+    console.log(data);
+
+    data.forEach((element) => {
+      console.log(element.username);
+    });
+
+    // tänne tiedot
+    const tbody = document.querySelector('.tbody');
+    tbody.innerHTML = '';
+
+    data.forEach((element) => {
+      console.log(element.username);
+
+      // Create table row element
+      var tr = document.createElement('tr');
+
+      // td1 Username
+      var td1 = document.createElement('td');
+      td1.innerText = element.username;
+
+      // td2
+      var td2 = document.createElement('td');
+      td2.innerText = element.user_level;
+
+      // td3
+      var td3 = document.createElement('td');
+      var button1 = document.createElement('button');
+      button1.className = 'check';
+      button1.setAttribute('data-id', '1');
+      button1.innerText = 'Info';
+      td3.appendChild(button1);
+
+      // td4
+      var td4 = document.createElement('td');
+      var button2 = document.createElement('button');
+      button2.className = 'del';
+      button2.setAttribute('data-id', '1');
+      button2.innerText = 'Delete';
+      td4.appendChild(button2);
+
+      // td5
+      var td5 = document.createElement('td');
+      td5.innerText = element.user_id;
+
+      // Append table data elements to the table row element
+      tr.appendChild(td1);
+      tr.appendChild(td2);
+      tr.appendChild(td3);
+      tr.appendChild(td4);
+      tr.appendChild(td5);
+
+      // Append the table row element to the table (assuming you have a table with the id 'myTable')
+      tbody.appendChild(tr);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
